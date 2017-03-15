d5f949c346107486e2811bad2721000296d9417e
fanzK12 <835258532@qq.com>
2017-03-15 11:10:06 +0800

    Initial Commit

---
+++ FKSwitchCardsDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FKSwitchCardsDemo
+//
+//  Created by apple on 17/3/15.
+//  Copyright © 2017年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
